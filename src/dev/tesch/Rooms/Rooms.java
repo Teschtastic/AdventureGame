6e21b41fc21ff1a39a1d54159227ea497d134f85
Sean <teschtastic@gmail.com>
2020-06-11 19:34:07 -0700

    Quick fix
    - Moves were opposite for two rooms, swapped those

--- src/dev/tesch/Rooms/Rooms.java
+++ src/dev/tesch/Rooms/Rooms.java
@@ -13,8 +13,8 @@ public class Rooms {
         roomsMap.put(1, new Room(1, new int[]{0, 1, 1, 0}, new int[] {-1, 3, 2, -1}, "Sean's bedroom"));
         roomsMap.put(2, new Room(2, new int[]{1, 0, 0, 0}, new int[] {1, -1, -1, -1}, "Sean's bathroom"));
         roomsMap.put(3, new Room(3, new int[]{1, 1, 1, 1}, new int[] {6, 5, 4, 1}, "Apartment inner hallway"));
-        roomsMap.put(4, new Room(4, new int[]{0, 0, 0, 1}, new int[] {3, -1, -1, -1}, "Jack's bathroom"));
-        roomsMap.put(5, new Room(5, new int[]{1, 0, 0, 0}, new int[] {-1, -1, -1, 3}, "Jack's bedroom"));
+        roomsMap.put(4, new Room(4, new int[]{1, 0, 0, 0}, new int[] {3, -1, -1, -1}, "Jack's bathroom"));
+        roomsMap.put(5, new Room(5, new int[]{0, 0, 0, 1}, new int[] {-1, -1, -1, 3}, "Jack's bedroom"));
 
         roomsMap.put(6, new Room(6, new int[]{1, 1, 1, 1}, new int[] {8, 7, 3, 9}, "Apartment outer hallway"));
         roomsMap.put(7, new Room(7, new int[]{0, 0, 0, 1}, new int[] {-1, -1, -1, 6}, "Apartment dining room"));
